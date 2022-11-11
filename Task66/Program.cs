ecd43b3e6d93904c8d8215cb652760c5acbb82a2
leonid ivanov <leonid.ivanov.w@gmail.com>
2022-11-11 18:04:45 +0500

    Task 66 done

---
+++ Task66/Program.cs
@@ -0,0 +1,11 @@
+﻿int n = Convert.ToInt32(Console.ReadLine());
+int m = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(SumBetweenNAndM(n, m));
+int SumBetweenNAndM(int n, int m)
+{
+    if (n >= m) return n;
+    else
+    {
+        return n + SumBetweenNAndM(n + 1, m);
+    }
+}
