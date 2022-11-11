86981ee4320f71ebc53dcce567cf4cb6a9f9c455
leonid ivanov <leonid.ivanov.w@gmail.com>
2022-11-11 18:23:12 +0500

    Task 68 done

---
+++ Task68/Program.cs
@@ -0,0 +1,11 @@
+﻿System.Console.WriteLine("Enter m:");
+int m = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Enter n:");
+int n = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(Ackermann(m, n));
+int Ackermann(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if (m > 0 && n == 0) return Ackermann(m - 1, 1);
+    else return Ackermann(m - 1, Ackermann(m, n - 1));
+}
