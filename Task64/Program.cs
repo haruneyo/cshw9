dcd7b925db8dd3c0a64aaeabd3dba87c50ec14ef
leonid ivanov <leonid.ivanov.w@gmail.com>
2022-11-11 17:54:40 +0500

    Task 64 done

---
+++ Task64/Program.cs
@@ -0,0 +1,11 @@
+﻿int n = Convert.ToInt32(Console.ReadLine());
+NumsFromNToOne(n);
+void NumsFromNToOne(int num)
+{
+    if (num == 1) System.Console.WriteLine(num);
+    else
+    {
+        System.Console.WriteLine(num);
+        NumsFromNToOne(num - 1);
+    }
+}
